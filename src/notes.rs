59faf3f6945e5c8f0dfa3c2353623cdaad2fb4b7
Mandar Vaze <mandarvaze@gmail.com>
2025-01-22 18:49:22 +0530

    + save contents of the note to 💾

--- src/notes.rs
+++ src/notes.rs
@@ -1,9 +1,17 @@
 use crate::common;
+use std::fs;
+use std::io::Write;
 
 pub fn add_note(content: String, filename: Option<String>, title: Option<String>) {
+    let home_dir = common::init::get_home_dir();
     let filename = common::utils::get_filename(filename);
+    let full_path = home_dir.join(filename);
     println!("Adding Note with Content {}", content);
-    println!("Saving to {}", filename);
+    println!("Saving to {}", full_path.display());
+
+    let mut file = fs::File::create(&full_path).expect("Unable to create file");
+    file.write_all(content.as_bytes())
+        .expect("Unable to write data");
 }
 
 pub fn view_note(id: &u32) {
