8651ca679c9c1b8806dace019b17de65a43f5279
Mandar Vaze <mandarvaze@gmail.com>
2025-01-26 12:59:54 +0530

    🔨 create .index file during initialization

--- src/common/init.rs
+++ src/common/init.rs
@@ -13,13 +13,24 @@ pub fn get_home_dir() -> PathBuf {
 /// The home directory is created at ~/.snb/home.
 ///
 /// If the directory already exists, a message indicating as such is printed.
+///
+/// Inside the home directory, empty `.index` file is created, if it doesn't already exist.
+///
 pub fn init() {
     let home_dir = get_home_dir();
 
     if !Path::new(&home_dir).exists() {
-        fs::create_dir_all(&home_dir).expect("Failed to create directory");
+        fs::create_dir_all(&home_dir).expect("Failed to create home directory");
         println!("Created directory: {:?}", home_dir);
     } /* else {
           println!("Directory already exists: {:?}", home_dir);
+      }*/
+
+    let index_file = home_dir.join(".index");
+    if !Path::new(&index_file).exists() {
+        fs::File::create(&index_file).expect("Failed to create index file");
+        println!("Created index file: {:?}", index_file);
+    } /* else {
+          println!(".index file already exists");
       }*/
 }
